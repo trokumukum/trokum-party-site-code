56e3699b984f53bd744de634876f8188621103de
trokumukum <rb@rotechnc.us>
2025-11-28 17:21:47 -0500

    Proving this ain't no virus.

---
+++ script.js
@@ -0,0 +1,9 @@
+function show() {
+    let scryimg = document.getElementById("scryimg");
+
+    scryimg.src = "fnafscary.gif"
+    scryimg.style.width = "50%";
+
+    document.getElementById("viewbtn")
+        .style.display = "none";
+}
